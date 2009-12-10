f8e22baf3038dec34859c5f612e5fb0be799f44c
Qingyang Xu <qingyang.xu@qunar.com>
2009-12-11 00:52:02 +0800

    修正task_controller里的bugs

--- rails_root/app/controllers/task_controller.rb
+++ rails_root/app/controllers/task_controller.rb
@@ -2,6 +2,7 @@ require 'uri'
 require 'open-uri'
 
 class TaskController < ApplicationController
+  @@valid_suffixes = {:html => 1, :htm => 1}
   def create
     @task = Task.new
   end
@@ -26,17 +27,25 @@ class TaskController < ApplicationController
     queue = [seed_url]
     Thread.new do
       download_count = 0
-      urls_found = {seed_url}
+      urls_found = [seed_url]
       while !queue.empty and download_count < number do
         url = queue.shift
-        html = open(url).read
-        page = Page.new :url => url, :html => html, :size = html.size
+        begin
+          html = open(url).read
+        rescue
+          next
+        end
+        page = Page.new :url => url, :html => html, :size => html.size
         task.pages << page
-        urls_found_in_this_page = URI.extract(html)
-        for url_found in urls_found_in_this_page do 
-          next if urls_found{url_found}
-          urls_found{url_found} = 1
-          queue << url_found
+        uris_found_in_this_page = URI.extract(html)
+        for new_uri in uris_found_in_this_page do 
+          next if urls_found[new_uri]
+          next unless new_uri =~ /^http:/
+          if new_uri =~ /\.([^.\/]+)$/ 
+            next unless @@valid_suffixes[$1.downcase]
+          end
+          queue << new_uri
+          urls_found[new_uri] = 1
         end
       end
     end
